1136943f15ebc4cb748875d8d69831af3c7fb187
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-12-19 12:52:48 -0200

    Add examples

---
+++ AwesomeProject/.bash_profile
@@ -0,0 +1,5 @@
+export ANDROID_HOME=/Users/gabrielscalici/Library/Android/sdk
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+export PATH=$ANDROID_HOME/tools:$PATH
+
+
---
+++ Test/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => HelloWorldApp);
