03fb59e149a8c289d9a6a94130b98b835836f6f5
GabrielScalici <gabrielhc.scalici@gmail.com>
2018-08-05 17:44:15 -0300

    telas navigation criadas

---
+++ Apps/Coach/src/model/Metas.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import {metrics, font, colors } from '../styles';
+
+class Metas extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    Metas
+                </Text>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {    
+        flex: 1,
+    },
+    title: {
+        fontSize: font.title,
+    }
+})
+
+export default Metas;
+
