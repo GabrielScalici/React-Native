ebe4ca28867762abd8217823c7e1651fbd1e65d1
GabrielScalici <gabrielhc.scalici@gmail.com>
2018-08-17 10:37:27 -0300

    link realizado

--- Apps/Coach/android/app/src/main/java/com/coach/MainApplication.java
+++ Apps/Coach/android/app/src/main/java/com/coach/MainApplication.java
@@ -3,6 +3,7 @@ package com.coach;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new LinearGradientPackage(),
             new RealmReactPackage(),
             new VectorIconsPackage()
