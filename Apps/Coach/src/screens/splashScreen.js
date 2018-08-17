d92304168a03a7d3ab81010e69da0265007a6a8c
GabrielScalici <gabrielhc.scalici@gmail.com>
2018-08-17 11:08:20 -0300

    login and splash Screen add

---
+++ Apps/Coach/src/screens/splashScreen.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import { metrics, font, colors } from '../styles';
+
+
+class splashScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.background_claro,
+    }
+
+})
+
+export default splashScreen;
